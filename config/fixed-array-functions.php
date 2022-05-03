5e5429d76adc442a83bc661d957fb84215b45f89
Oliver Earl <oliver@oliverearl.co.uk>
2022-05-03 22:40:49 +0100

    Remove unneeded files.
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- config/fixed-array-functions.php
+++
@@ -1,5 +0,0 @@
-<?php
-// config for Petrobolos/FixedArrayFunctions
-return [
-
-];
--- src/FixedArrayFunctionsServiceProvider.php
+++ src/FixedArrayFunctionsServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Petrobolos\FixedArray;
 
-use Petrobolos\FixedArray\Commands\FixedArrayFunctionsCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +9,6 @@ class FixedArrayFunctionsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('fixed-array-functions')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_fixed-array-functions_table')
-            ->hasCommand(FixedArrayFunctionsCommand::class);
+        $package->name('fixed-array-functions');
     }
 }
